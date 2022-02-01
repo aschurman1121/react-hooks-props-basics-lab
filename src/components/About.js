dff5f0cfeb7dc8d7ddfb34410be97b0076f9311d
alexandra <alexandraschurman@gmail.com>
2022-01-31 16:31:59 -0800

    commit tree d9829ce822cb55e83c829d98566346a1219c67ac

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from "react";
+import user from '../data/user';
 
-function About() {
+function Links(props) {
+
+  const links = user.links
+
+  return (
+    <><h3>Links</h3>
+    <a href={links.github}> {links.github} </a>
+    <a href={links.linkedin}> {links.linkedin} </a></>
+  )
+}
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links />}
     </div>
   );
 }
